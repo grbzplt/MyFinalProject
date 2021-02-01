7227bb2791c32ab83b7f1cbe58dd19036648342f
Grbz Plt <grbzplt@hotmail.com>
2021-02-01 18:21:39 +0300

    Güncelleme

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -12,6 +12,7 @@ namespace DataAccess.Concrete.InMemory
         List<Product> _products;
         public InMemoryProductDal()
         {
+            //Sql Server, Oracle, MongoDb geliyormuş gibi simule ediyoruz.
             _products = new List<Product> {
                 new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
                 new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
@@ -20,31 +21,47 @@ namespace DataAccess.Concrete.InMemory
                 new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
             };
         }
+
         public void Add(Product product)
         {
             _products.Add(product);
             
         }
+
+        //public void Delete(Product product)
+        //{
+        //    //(1) 
+        //    _products.Remove(product); //Bu şekilde product silinmez.Çünkü referans lar ile çalışıyoruz.
+
+        //    //Ürün silerken onun primary key ini kullanırız.
+
+        //    //(2) LINQ kullanmadan klasik yöntemle:
+        //    //Product productToDelete=new Product() --> HATALI kullanım;
+        //    Product productToDelete=null; //null --> referansı yok.
+        //    foreach (var p in _products)
+        //    {
+        //        if (product.ProductId==p.ProductId)
+        //        {
+        //            productToDelete = p;
+        //        }
+        //    }
+        //   _products.Remove(productToDelete);          
+        //}
+
         public void Delete(Product product)
         {
-            ////klasik yöntem:
-            //Product productToDelete=null;
-            //foreach (var p in _products)
-            //{
-            //    if (product.ProductId==p.ProductId)
-            //    {
-            //        productToDelete = p;
-            //    }
-            //}
-            //_products.Remove(productToDelete);
+            //(3) LINQ kullanarak:
 
-            //LINQ
             Product productToDelete;
 
-            productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            //SingleOrDefault --> tek bir eleman bulmaya yarar. (FirstOrDefault  da kullanılabilir.)
+            //SingleOrDefault(p=>p.ProductId==product.ProductId)  kodu yukarıdaki foreach in yaptığını yapıyor.
+
+            productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
 
             _products.Remove(productToDelete);
         }
+
         public void Update(Product product)
         {
             //Gönderdiğim ürün Id sine sahip olan listedeki ürünü bul
